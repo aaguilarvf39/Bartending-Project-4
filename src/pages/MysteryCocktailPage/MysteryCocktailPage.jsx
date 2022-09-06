cc2f02deafa6cd3678e5e50fe80349ff10134481
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-09-05 18:10:17 -0700

    adding ingredients and measures

--- src/pages/MysteryCocktailPage/MysteryCocktailPage.jsx
+++ src/pages/MysteryCocktailPage/MysteryCocktailPage.jsx
@@ -15,13 +15,37 @@ export function MysteryCocktailPage() {
                     idDrink,
                     strDrink,
                     strInstructions,
-                    strDrinkThumb
+                    strDrinkThumb,
+                    strIngredient1,
+                    strIngredient2,
+                    strIngredient3,
+                    strIngredient4,
+                    strIngredient5,
+                    strIngredient6,
+                    strMeasure1,
+                    strMeasure2,
+                    strMeasure3,
+                    strMeasure4,
+                    strMeasure5,
+                    strMeasure6
     
                 } = m
             return (
                 <div key={idDrink}>
                     <h1>{strDrink}</h1>
                     <h2>{strInstructions}</h2>
+                    <h3>{strIngredient1}</h3>
+                    <h3>{strIngredient2}</h3>
+                    <h3>{strIngredient3}</h3>
+                    <h3>{strIngredient4}</h3>
+                    <h3>{strIngredient5}</h3>
+                    <h3>{strIngredient6}</h3>
+                    <h3>{strMeasure1}</h3>
+                    <h3>{strMeasure2}</h3>
+                    <h3>{strMeasure3}</h3>
+                    <h3>{strMeasure4}</h3>
+                    <h3>{strMeasure5}</h3>
+                    <h3>{strMeasure6}</h3>
                     <img src={strDrinkThumb}></img>
                 </div>
             )
