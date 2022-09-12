29ee1a36bdab0fb4eb821634ddb1d6b3070345df
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-09-11 20:25:28 -0700

    css done for now

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,11 @@ import './Home.css'
 
 export default function Home() {
     return (
-        <div className='Home'>
-            <img src="https://imgur.com/OygmPCC.jpg"></img>
+        <>
             <h1 className='title'>Welcome one welcome all! Sit back, relax and discover some neat cocktails!</h1>
-        </div>
+            <img className="Home" src="https://imgur.com/OygmPCC.jpg"></img>
+            <img className="pic" src="https://imgur.com/Ca28W3J.jpg"></img>
+            <h1 className='gts'>Green Tea Shots</h1>
+        </>
     )
 }
